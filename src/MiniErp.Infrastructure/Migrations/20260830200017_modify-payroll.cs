5cb2f9207fb9331e0adbae010df93afdf1367b7b
user <user@WIN-ST2GVRTI94F>
2026-08-30 23:01:08 +0300

    update

---
+++ src/MiniErp.Infrastructure/Migrations/20260830200017_modify-payroll.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniErp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifypayroll : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_PayrollEntries_Amounts_NonNegative",
+                table: "PayrollEntries");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_PayrollEntries_Amounts_NonNegative",
+                table: "PayrollEntries",
+                sql: "([Overtimebydayunit] IS NULL OR [Overtimebydayunit] >= 0) AND ([Deductionbydayunit] IS NULL OR [Deductionbydayunit] >= 0) AND ([RequiredWorkingDays] IS NULL OR [RequiredWorkingDays] >= 0) AND ([Bonus] IS NULL OR [Bonus] >= 0) AND ([Deduction] IS NULL OR [Deduction] >= 0) AND ([SalaryPerDay] IS NULL OR [SalaryPerDay] >= 0) AND [CalculatedSalary] >= 0 AND [GrossSalary] >= 0 ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_PayrollEntries_Amounts_NonNegative",
+                table: "PayrollEntries");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_PayrollEntries_Amounts_NonNegative",
+                table: "PayrollEntries",
+                sql: "([Overtimebydayunit] IS NULL OR [Overtimebydayunit] >= 0) AND ([Deductionbydayunit] IS NULL OR [Deductionbydayunit] >= 0) AND ([RequiredWorkingDays] IS NULL OR [RequiredWorkingDays] >= 0) AND ([Bonus] IS NULL OR [Bonus] >= 0) AND ([Deduction] IS NULL OR [Deduction] >= 0) AND ([SalaryPerDay] IS NULL OR [SalaryPerDay] >= 0) AND [CalculatedSalary] >= 0 AND [GrossSalary] >= 0 AND [NetSalary] >= 0");
+        }
+    }
+}
